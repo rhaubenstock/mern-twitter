354ceaf3b6a33ef796f4fde6ffa84fb870426c7e
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-02-20 23:09:18 -0800

    Add test routes for tweets and users

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
-
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to mongoDB"))
@@ -11,6 +12,9 @@ app.get("/", (req, res) => {
   res.send("Hello Worlddddd!");
 });
 
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
