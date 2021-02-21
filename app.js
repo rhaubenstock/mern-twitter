c646e4128a955990bb5c42e370a38efc3180eada
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-02-21 12:52:35 -0800

    Add User model and register route

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const User = require('./models/User');
 
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to mongoDB"))
   .catch(err => console.log(err));
+
+
 app.get("/", (req, res) => {
+  const user = new User ({
+    handle: "jim",
+    email: "jim@jim.jim",
+    password: "jimisgreat123"
+  });
+  user.save();
   res.send("Hello Worlddddd!");
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
